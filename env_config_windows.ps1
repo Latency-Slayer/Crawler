5074017b45fbebb7dd4011d356b3319db997dac6
BryanRO-SpTech <bryan.grocha@sptech.school>
2025-04-03 20:57:52 -0300

    verify_python

---
+++ env_config_windows.ps1
@@ -0,0 +1,20 @@
+﻿echo "Configurando seu ambiente..."
+
+echo "Verificando dependencias..."
+
+
+try {
+    $pythonVersion = python --version 2>$null  # Tenta executar o Python
+    if ($?) {
+        echo "Python encontrado: $pythonVersion"
+    } else {
+        throw "Python não está instalado. Instale o python."
+
+
+    }
+} catch {
+    echo "Erro: Python não está instalado ou não está no PATH."
+    exit 1  
+}
+
+
